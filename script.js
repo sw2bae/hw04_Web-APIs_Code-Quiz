d6e2314e49be5b45d8d9377ff055212a60edead9
Sang Bae <sw2.bae@gmail.com>
2020-04-03 22:34:19 -0500

    hw04_rev01_html/js

---
+++ script.js
@@ -0,0 +1,37 @@
+var questionsArray = [
+    { quesiton : "question 1",
+    
+    choice : ["1","2","3","4","5"],
+
+    answer : "1"
+    },
+    { quesiton : "question 2",
+    
+    choice : ["1","2","3","4","5"],
+
+    answer : "2"
+    },
+    { quesiton : "question 3",
+    
+    choice : ["1","2","3","4","5"],
+
+    answer : "3"
+    },
+    { quesiton : "question 4",
+    
+    choice : ["1","2","3","4","5"],
+
+    answer : "4"
+    },
+    { quesiton : "question 5",
+    
+    choice : ["1","2","3","4","5"],
+
+    answer : "5"
+    }
+]; 
+    
+
+
+
+
